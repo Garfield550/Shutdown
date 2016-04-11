90632114e4c1ec4bee2436937c48fc8b4e0ee0ca
Stark Lee <garfield_550@sina.cn>
2016-04-11 12:31:29 +0800

    Version: 1.0.0411.2

---
+++ Shutdown/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using System.Management;
+using System.Runtime.InteropServices;
+
+namespace Shutdown
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Shutdown(string flags, string reserved)
+        {
+            ManagementBaseObject mboShutdown = null;
+            ManagementClass mcWin32 = new ManagementClass("Win32_OperatingSystem");
+            mcWin32.Get();
+            // You can't shutdown without security privileges
+            mcWin32.Scope.Options.EnablePrivileges = true;
+            ManagementBaseObject mboShutdownParams = mcWin32.GetMethodParameters("Win32Shutdown");
+            // Flag 1 means we want to shut down the system
+            mboShutdownParams["Flags"] = flags;
+            mboShutdownParams["Reserved"] = reserved;
+            foreach (ManagementObject manObj in mcWin32.GetInstances())
+            {
+                mboShutdown = manObj.InvokeMethod("Win32Shutdown", mboShutdownParams, null);
+            }
+        }
+
+        private void buttonShutdown_Click(object sender, RoutedEventArgs e)
+        {
+            Shutdown("8", "0");
+        }
+
+        private void buttonReboot_Click(object sender, RoutedEventArgs e)
+        {
+            Shutdown("2", "0");
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool LockWorkStation();
+        private void buttonLock_Click(object sender, RoutedEventArgs e)
+        {
+            LockWorkStation();
+        }
+
+        [DllImport("Powrprof.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern bool SetSuspendState(bool hiberate, bool forceCritical, bool disableWakeEvent);
+        private void buttonHibernate_Click(object sender, RoutedEventArgs e)
+        {
+            SetSuspendState(true, true, true);
+        }
+    }
+}
